e948fed1db298829239cee99593a94217147bec0
Dave Lasley <dave@laslabs.com>
2015-12-31 19:36:37 -0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,19 @@
+Odoo Base Module Enhancements
+========================
+
+Extensions enhancing base Odoo functionality such as partners and users
+
+
+Credits
+=======
+
+Contributors
+------------
+
+* Dave Lasley <dave@laslabs.com>
+
+Maintainer
+----------
+
+This module is maintained by LasLabs Inc.
+https://repo.laslabs.com/
---
+++ res_partner_multi_address/__openerp__.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Copyright (C) LasLabs, Inc [https://laslabs.com]. All Rights Reserved
+#
+##############################################################################
+#
+#    Collaborators of this module:
+#       Written By: Dave Lasley <dave@laslabs.com>
+#
+##############################################################################
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+{
+
+    'name': 'Partner Multiple Addresses',
+    'version': '8.0.1.0.0',
+    'author': "LasLabs",
+    'category': 'Base',
+    'depends': [
+    ],
+    "website": "https://laslabs.com",
+    "licence": "AGPL-3",
+    "data": [
+    ],
+    'test': [
+    ],
+    'installable': True,
+    'auto_install': False,
+}
