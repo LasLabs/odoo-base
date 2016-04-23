2195364e99b6d901a9658b50ebc0f5f802bc44ed
Dave Lasley <dave@laslabs.com>
2016-04-23 11:40:04 -0700

    Start smime

--- mail_smime/__openerp__.py
+++ mail_smime/__openerp__.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
-# © 2015-TODAY LasLabs Inc.
+# © 2015 LasLabs Inc.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
 {
-    'name': 'Encrypted Fields Module For Odoo',
-    'version': '8.0.0.1',
+    'name': 'SMIME module for Odoo Mail',
+    'description': 'Allows for proper handling of SMIME emails in Odoo',
+    'version': '9.0.1.0.0',
     'category': '',
     'author': "LasLabs",
     'license': 'AGPL-3',
--- mail_smime/models/import_email.py
+++ mail_smime/models/import_email.py
@@ -1,23 +1,6 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Author: Dave Lasley <dave@laslabs.com>
-#    Copyright: 2015 LasLabs, Inc.
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# © 2015 LasLabs Inc.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from openerp.tools.import_email import ReceiverEmail2Event
 from tempfile import TemporaryFile
@@ -44,7 +27,7 @@ class ReceiverEmail2Event(ReceiverEmail2Event):
         smime.set_x509_store(st)
         self._smime = smime
     
-    def validate_smime(self, msg_part, ):
+    def validate_smime(self, msg_part):
         '''
         Validates SMIME certs
         :param  msg_part: email.message
